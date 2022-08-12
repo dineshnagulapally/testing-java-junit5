59a24f1f90d08107c790e2f3a56bb83699691b9d
dinesh <dineshnagulapally@gmail.com>
2022-08-12 12:40:37 -0500

    Default methods in test interfaces using @TestInstance(TestInstance.LifeCycle.PER_CLASS)

--- src/test/java/guru/springframework/Controller.java
+++ src/test/java/guru/springframework/Controller.java
@@ -1,8 +1,17 @@
 package guru.springframework;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("controller")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//it will not be able to run the test case in this interface unless we make it @TestInstance
 public interface Controller {
+
+    @BeforeAll
+    default void beforeAll(){
+        System.out.println("before all");
+    }
 }
