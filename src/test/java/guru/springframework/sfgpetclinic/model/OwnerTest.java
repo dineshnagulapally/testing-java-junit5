b042bc284fb6dd4681f63daebe91a187b1444904
dinesh <dineshnagulapally@gmail.com>
2022-08-12 22:44:22 -0500

    @ParametrizedTest with Value source. Added <dependency>                                                        <groupId>org.junit.jupiter</groupId>                                                        <artifactId>junit-jupiter-engine</artifactId>                                                        <version>${junit-platform.version}</version>                                           </dependency>

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgpetclinic.model;
 import guru.springframework.sfgpetclinic.ModelInterface;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -33,5 +35,11 @@ class OwnerTest implements ModelInterface {
 
         );
         assertThat(owner.getCity(),is("Key West"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Spring", "Framework"})
+    void testValueSource(String val){
+        System.out.println(val);
     }
 }
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -32,4 +32,5 @@ class PersonTest implements ModelInterface {
     }
 
 
+
 }
